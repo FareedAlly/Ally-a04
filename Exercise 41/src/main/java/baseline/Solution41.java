cde193bfcd19bd5bd2bb4bacc4bf847dcddb5c6f
Fareed <fareedally1999@gmail.com>
2021-10-15 12:50:22 -0400

    added exercise 41 pseudocode

---
+++ Exercise 41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,43 @@
+package baseline;
+
+import java.util.List;
+
+public class Solution41
+{
+    private List<String> readNames(String input)
+    {
+        // Open input file
+        // Add the first and last names to a list
+        // Return list
+    }
+
+    private void sort(List<String> names)
+    {
+        // Compare two names at a time alphabetically
+        // If second name is earlier than first, swap them
+        // Keep doing this until every name is sorted (WHILE LOOP?)
+    }
+
+    private String output(List<String> names)
+    {
+        // Call sort function on list
+        // Add the static print statements (USE STRINGBUILDER?)
+        // Send the newly ordered names to the list
+        // Return String(or list)
+    }
+
+    private void sendOutputToFile(String output)
+    {
+        // Open output file
+        // Print output string to output file
+    }
+
+    public static void main(String[] args)
+    {
+        // Create list of names from input file
+        // Create output string by calling the functions
+        // Print output string to output file
+
+    }
+
+}
