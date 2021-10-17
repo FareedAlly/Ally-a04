7e1d030769e86b4b0de2eeb32abf2cba435a627e
Fareed <fareedally1999@gmail.com>
2021-10-17 13:30:30 -0400

    added exercise 42 pseudocode

---
+++ Exercise 42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,33 @@
+package baseline;
+
+import java.util.List;
+
+public class Solution42
+{
+    private List<String> readInput(String input)
+    {
+        // Use a filereader and a bufferedreader to read the file
+        // Create a list
+        // Use while loop to go through each line and assign it to a string
+        // Add those to the list
+        // Return list
+    }
+
+    private void output(List<String> names)
+    {
+        // Use filewriter to write to the output file
+        // Print the static header for table
+        // Go through the list and print contents
+        // Close filewriter
+    }
+
+    public static void main(String[] args)
+    {
+        Solution42 main = new Solution42();
+
+        // Call readInput function and assign the result to a list
+        // Call output function and create an outFile with the results
+
+    }
+
+}
