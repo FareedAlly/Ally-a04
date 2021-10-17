4c4b2204d0e4e9a0f1e8b439469ee7b5fbf67f03
Fareed <fareedally1999@gmail.com>
2021-10-17 19:20:41 -0400

    added exercise 43 pseudocode

---
+++ Exercise 43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,57 @@
+package baseline;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Solution43
+{
+    private boolean input(String prompt)
+    {
+        // Print out prompt
+        // Use if statements to get values when user enters Y or N
+        // Only allow these by using while loop?
+    }
+
+    public static void main(String[] args)
+    {
+        // Ask user for website name and author
+        // Ask user if they want folder for java and css
+        // Call website class
+        // Create website
+
+    }
+
+}
+
+class website
+{
+    private websiteGenerator(String name, String author, boolean jsFolder, boolean cssFolder)
+    {
+        // Assign values from solution43 Class to this one
+    }
+
+    private void generate()
+    {
+        // Create root folder
+        // Create index (FUNCTION?)
+        // Make folder for js or css depending on what user chooses
+    }
+
+    private void generateIndex()
+    {
+        // Create the header, heading, author, title etc...
+        // How tho
+        // ???
+        // Profit
+    }
+
+    private void generateDirectory()
+    {
+        // Create path to new directory
+        // Display path to new directory 
+    }
+
+
+
+
+}
