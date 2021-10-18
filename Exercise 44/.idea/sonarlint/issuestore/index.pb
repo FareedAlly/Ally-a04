1ffc30e942df77bf1c45e8ad022fb7815d6fb1c6
Fareed <fareedally1999@gmail.com>
2021-10-17 22:17:03 -0400

    added exercise 45 pseudocode

---
+++ Exercise 44/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+J
+data/exercise44_input.json,c\f\cfcd15bd9321f4c3f0770cd865d4568c14c53f8e
+A
+docs/diagram.puml,f\1\f1d4fe943c70db8e17c963b3b283a5d7df2524a2
+V
+&src/main/java/baseline/Solution44.java,5\e\5ed8bc95bcdb167ab4dfbdf85c9a8ea80c8aa4ce
+Z
+*src/test/java/baseline/Solution44Test.java,c\b\cba3fa9d6fef2aa32ea57e972cb61588f705d45e
---
+++ Exercise 45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,36 @@
+package baseline;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Solution45
+{
+    private void search(String inFile, String outFile)
+    {
+        // Read inFile
+        // Call other function to replace utilize with use
+    }
+
+    private void replace(Scanner in, String outFile)
+    {
+        // Create outFile
+        // Read file line by line
+        // Replace "utilize" with "use"
+        // Print to outFile
+    }
+
+    private Path getPath(String name)
+    {
+        // Return directory
+    }
+
+    public static void main(String[] args)
+    {
+        Solution45 main = new Solution45();
+
+        // Ask user for outFile name
+        // Replace the words with the functions
+
+    }
+
+}
