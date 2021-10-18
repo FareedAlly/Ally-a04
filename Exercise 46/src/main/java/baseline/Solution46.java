7d9c452a6b690bf4fff77fef38449b98b7f83984
Fareed <fareedally1999@gmail.com>
2021-10-17 23:03:41 -0400

    added exercise 46 pseudocode

---
+++ Exercise 46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,40 @@
+package baseline;
+
+import java.util.Map;
+
+public class Solution46
+{
+    private Map<String, Integer> search()
+    {
+        // Open infile
+        // Read each word from file
+        // Compare to keys on map
+        // If they are same set value to +1
+        // If not same add new key with value of 1
+        // Return map
+    }
+
+    private Map<String, Integer> sort(Map<String, Integer> map)
+    {
+        // Create new map with new values gathered from search function
+    }
+
+    private String createHistogram(Map<String, Integer> sortedMap)
+    {
+        // Use a for loop to create histogram
+        // Also StringBuiler
+        // Return string
+    }
+
+    public static void main(String[] args)
+    {
+        Solution46 main = new Solution46();
+
+        // Create a map
+        // Sort the map using function
+        // Assemble using function
+        // Print output
+
+    }
+
+}
