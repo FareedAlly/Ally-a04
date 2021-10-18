ae3d7d94c3ac4f094f62420d1a122c5030f98bb3
Fareed <fareedally1999@gmail.com>
2021-10-17 20:21:01 -0400

    completed exercise 43

--- Exercise 43/src/main/java/baseline/Solution43.java
+++ Exercise 43/src/main/java/baseline/Solution43.java
@@ -1,54 +1,184 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Fareed Ally
+ */
+
 package baseline;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Formatter;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Solution43
 {
+    private static final Scanner in = new Scanner(System.in);
+
     private boolean input(String prompt)
     {
+        String answer;
+
         // Print out prompt
-        // Use if statements to get values when user enters Y or N
-        // Only allow these by using while loop?
+        while(true)
+        {
+            System.out.println(prompt);
+
+            answer = in.nextLine().toUpperCase(Locale.ROOT);
+
+            // Use if statements to get values when user enters Y or N
+
+            if(answer.equals("Y"))
+            {
+                return true;
+            }
+            else if(answer.equals("N"))
+            {
+                return false;
+            }
+            // Only allow these by using while loop
+            else
+            {
+                System.out.println("Enter Y for yes or N for no");
+            }
+        }
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        // Ask user for website name and author
-        // Ask user if they want folder for java and css
-        // Call website class
-        // Create website
+        Solution43 main = new Solution43();
 
+        String websiteName;
+        String authorName;
+        boolean jsFolder;
+        boolean cssFolder;
+
+        // Ask user for website name and author
+        System.out.println("Site name: ");
+        websiteName = in.nextLine();
+
+        System.out.println("Author: ");
+        authorName = in.nextLine();
+
+        // Ask user if they want folder for java and css
+        jsFolder = main.input("Do you want a folder for JavaScript? ");
+
+        cssFolder = main.input("Do you want a folder for CSS? ");
+
+        // Call website class
+        website create = new website(websiteName, authorName, jsFolder, cssFolder);
+
+        // Create website
+        create.generate();
+
+        System.exit(0);
     }
 
 }
 
 class website
 {
-    private websiteGenerator(String name, String author, boolean jsFolder, boolean cssFolder)
+    private final Path currentPath = Paths.get(System.getProperty("user.dir"), "data");
+
+    private  Path siteRoot;
+
+    private final String websiteName;
+    private final String authorName;
+
+    private final boolean jsFolder;
+    private final boolean cssFolder;
+
+    public website(String name, String author, boolean jsFolder, boolean cssFolder)
     {
         // Assign values from solution43 Class to this one
+        this.websiteName = name;
+        this.authorName = author;
+
+        this.jsFolder = jsFolder;
+        this.cssFolder = cssFolder;
     }
 
-    private void generate()
+    public void generate() throws IOException
     {
         // Create root folder
+        siteRoot = Paths.get(currentPath.toString(), "website", websiteName);
+
+        boolean success = new File(siteRoot.toString()).mkdirs();
+
+        if(success)
+        {
+            System.out.println("Created " + siteRoot.toString().replaceFirst(currentPath.toString().replace("\\", "\\\\"), "."));
+        }
+        else
+        {
+            System.out.println("Unable to create directory in " + siteRoot.toString());
+        }
+
         // Create index (FUNCTION?)
+        generateIndex();
+
+
         // Make folder for js or css depending on what user chooses
+        if(jsFolder)
+        {
+            generateDirectory("js");
+        }
+        if(cssFolder)
+        {
+            generateDirectory("css");
+        }
     }
 
-    private void generateIndex()
+    private void generateIndex() throws IOException
     {
         // Create the header, heading, author, title etc...
-        // How tho
-        // ???
-        // Profit
+        String indexHeader = "index.html";
+
+        try(Formatter output = new Formatter(Paths.get(siteRoot.toString(), indexHeader).toString()))
+        {
+            output.format("<!DOCTYPE html>%n");
+
+            output.format("<html>%n");
+
+            output.format("<head>%n");
+
+            output.format("<meta charset=\"utf-8\">%n");
+
+            String authorMeta = String.format("<meta name = \"author\" content = \"%s\">%n", authorName);
+            output.format(authorMeta);
+
+            String title = String.format("<title>%s</title>%n", websiteName);
+            output.format(title);
+
+            output.format("</head>%n");
+
+            output.format("</html>%n");
+        }
+
+        System.out.println("Created " + Paths.get(siteRoot.toString(), indexHeader).toString().replaceFirst(
+                currentPath.toString().replace("\\", "\\\\"), "."));
     }
 
-    private void generateDirectory()
+    private void generateDirectory(String directoryName)
     {
         // Create path to new directory
-        // Display path to new directory 
+        Path newPath = Paths.get(siteRoot.toString(), directoryName);
+
+        boolean success = new File(newPath.toString()).mkdirs();
+
+        // Display path to new directory
+        if(success)
+        {
+            System.out.println("Created " + newPath.toString().replaceFirst(currentPath.toString().replace("\\", "\\\\"), "."));
+        }
+        else
+        {
+            System.out.println("Unable to create directory at " + newPath);
+        }
+
+
     }
 
 
